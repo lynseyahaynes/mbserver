139e45707d1961ad008281c0f640581dba6e875c
Lynsey Haynes <lynsey@dhcp-206-87-210-198.ubcsecure.wireless.ubc.ca>
2016-02-03 16:19:05 -0800

    Added get and set functionality

--- mbserver.go
+++ mbserver.go
@@ -17,6 +17,7 @@ import (
 )
 
 var rwmutex sync.RWMutex
+var kvmap map[string]string
 
 func checkError(err error) {
 	if err != nil {
@@ -25,22 +26,81 @@ func checkError(err error) {
 	}
 }
 
+// --------------------------------------------------------------------------
+
+// Send an error response to the connection and close it
 func handleUnknown(conn net.Conn) {
 	fmt.Println("received unknown message!")
+	res := gomemcached.MCResponse{
+		Opcode: gomemcached.NOOP,
+		Status: gomemcached.UNKNOWN_COMMAND,
+		Opaque: 0,
+		Cas:    0,
+		Extras: []byte(""),
+		Key:    []byte(""),
+	}
+
+	conn.Write(res.Bytes())
+	conn.Close()
 }
+
+// --------------------------------------------------------------------------
 
 func handleSet(req gomemcached.MCRequest, conn net.Conn) {
 	rwmutex.Lock()
 	fmt.Println("inside the write lock!")
+	key := string(req.Key[:len(req.Key)])
+	val := string(req.Body[:len(req.Body)])
+	kvmap[key] = val
+	res := gomemcached.MCResponse{
+		Opcode: gomemcached.SET,
+		Status: gomemcached.SUCCESS,
+		Opaque: 0,
+		Cas:    0,
+		Extras: []byte{0},
+		Key:    req.Key,
+		Body:   []byte{0},
+	}
+	conn.Write(res.Bytes())
 	rwmutex.Unlock()
 
 }
 
+// --------------------------------------------------------------------------
+
 func handleGet(req gomemcached.MCRequest, conn net.Conn) {
 	rwmutex.RLock()
-	fmt.Println("inside the read lock!")
+	key := string(req.Key[:len(req.Key)])
+	val := kvmap[key]
+
+	var res gomemcached.MCResponse
+	if val != "" {
+		res = gomemcached.MCResponse{
+			Opcode: gomemcached.GET,
+			Status: gomemcached.SUCCESS,
+			Opaque: 0,
+			Cas:    0,
+			Extras: []byte{0},
+			Key:    req.Key,
+			Body:   []byte(val),
+		}
+	} else {
+		res = gomemcached.MCResponse{
+			Opcode: gomemcached.GET,
+			Status: gomemcached.KEY_ENOENT,
+			Opaque: 0,
+			Cas:    0,
+			Extras: []byte{0},
+			Key:    req.Key,
+			Body:   []byte("Not found"),
+		}
+	}
+	conn.Write(res.Bytes())
+
 	rwmutex.RUnlock()
 }
+
+// --------------------------------------------------------------------------
 
 // Handles incoming GET and SET requests.
 func handleRequest(conn net.Conn) {
@@ -62,6 +122,8 @@ func handleRequest(conn net.Conn) {
 	fmt.Println(req)
 }
 
+// --------------------------------------------------------------------------
+
 //Listens for connections from possibly concurrent clients
 func listenForConnections(addr string) {
 	server, err := net.Listen("tcp", addr)
@@ -75,9 +137,12 @@ func listenForConnections(addr string) {
 	}
 }
 
+// --------------------------------------------------------------------------
+
 // Where the magic happens
 func main() {
 
+	kvmap = make(map[string]string)
 	addr := os.Args[1]
 	listenForConnections(addr)
 
